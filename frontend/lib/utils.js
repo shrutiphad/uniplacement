09589e451fece5ca9d16a7c32d32681e0370def6
shrutiphad <shrutiphadwork@gmail.com>
2026-06-24 23:51:54 +0530

    update resume

--- frontend/lib/utils.js
+++ frontend/lib/utils.js
@@ -27,7 +27,7 @@ export const STATUS_COLORS = {
 
 export const DEPARTMENTS = [
   'Computer Science', 'Information Technology', 'Electronics',
-  'Mechanical', 'Civil', 'Chemical', 
+  'Mechanical', 'Civil',
 ];
 
 export const SEMESTERS = [1, 2, 3, 4, 5, 6, 7, 8];
